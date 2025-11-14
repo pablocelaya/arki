2c0f2a8ee6b74364e3ab1dd5ee6a850d1b78acd7
pablocelaya <pablocelayarios@gmail.com>
2025-11-14 17:07:05 -0300

    deploymen backt

--- server-flask/app.py
+++ server-flask/app.py
@@ -140,5 +140,5 @@ def health():
 # ========================================================
 # Cambia esto al final:
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
